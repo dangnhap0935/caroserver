2e31e8817c180718e1bcd560df8feee40a02cb02
dangnhap0935 <70467981+dangnhap0935@users.noreply.github.com>
2020-12-11 18:15:58 +0700

    Add files via upload

---
+++ models/users.model.js
@@ -0,0 +1,21 @@
+var db = require('../utils/db');
+var bcrypt = require('bcrypt');
+
+module.exports = {
+
+    all: () => {
+        return db.load(`select * from users`);
+    },
+
+    add: entity => {
+        return db.add(`users`, entity);
+    },
+
+    get: username => {
+        return db.load(`select * from users where username = '${username}'`);
+    },
+
+    put: entity => {
+        return db.update(`users`, `username`, entity);
+    },
+}
